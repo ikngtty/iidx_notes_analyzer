5108cb5033ec147a2e184ad56cc13b56b541ce37
ikngtty <ikngtty@gmail.com>
2024-09-10 01:12:35 +0900

    スクレイピング処理をサブパッケージとして分離

---
+++ iidx_notes_analyzer/textage_scraper/main.py
@@ -0,0 +1,10 @@
+from playwright.sync_api import sync_playwright
+
+def scrape():
+    with sync_playwright() as pw:
+        browser = pw.chromium.launch()
+        page = browser.new_page()
+        page.goto('https://textage.cc/score/11/aa_amuro.html?1AC00')
+        notes = page.evaluate('npos')
+        browser.close()
+    return notes
--- scrape.py
+++ scrape.py
@@ -1,20 +1,10 @@
-from playwright.sync_api import sync_playwright
-
 from iidx_notes_analyzer import persistence
+from iidx_notes_analyzer.textage_scraper.main import scrape
 
 def main():
     notes = scrape()
     notes.sort()
     persistence.save(notes)
-
-def scrape():
-    with sync_playwright() as pw:
-        browser = pw.chromium.launch()
-        page = browser.new_page()
-        page.goto('https://textage.cc/score/11/aa_amuro.html?1AC00')
-        notes = page.evaluate('npos')
-        browser.close()
-    return notes
 
 if __name__ == '__main__':
     main()
