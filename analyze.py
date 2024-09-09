7c830993fd12a631ca47a0d8b4643931976b0ba8
ikngtty <ikngtty@gmail.com>
2024-09-10 01:17:48 +0900

    スクリプト内の関数を全部とりあえずutilに放り込む

--- analyze.py
+++ analyze.py
@@ -1,11 +1,11 @@
 from collections import Counter
-from itertools import combinations, groupby
+from itertools import combinations
 
-from iidx_notes_analyzer import persistence
+from iidx_notes_analyzer import persistence, util
 
 def main():
     notes = persistence.load()
-    chords = to_chords(notes)
+    chords = util.to_chords(notes)
     chord_counts = Counter(chords)
     for has_scratch in [False, True]:
         for note_count_in_chord in range(1, 8):
@@ -16,29 +16,8 @@ def main():
 
                 count = chord_counts[chord]
                 if count > 0:
-                    chord_str = chord_to_str(chord)
+                    chord_str = util.chord_to_str(chord)
                     print(f'{chord_str}:{count}')
-
-def to_chords(notes):
-    # TODO: 1Pの譜面か2Pの譜面かを考慮してない（特にDP）
-    chords = []
-    for _, notes_of_chord in groupby(notes, lambda note: note // 10):
-        chord = 0
-        for note in notes_of_chord:
-            chord |= (1 << (note % 10))
-        chords.append(chord)
-    return chords
-
-def chord_to_str(chord):
-    scratch_str = 'S' if chord & 1 == 1 else ' '
-    keys = chord >> 1
-    keys_str = reversed_str(f'{keys:07b}')\
-        .replace('1', '|')\
-        .replace('0', '_')
-    return scratch_str + keys_str
-
-def reversed_str(s):
-    return s[::-1]
 
 if __name__ == '__main__':
     main()
---
+++ iidx_notes_analyzer/util.py
@@ -0,0 +1,22 @@
+from itertools import groupby
+
+def to_chords(notes):
+    # TODO: 1Pの譜面か2Pの譜面かを考慮してない（特にDP）
+    chords = []
+    for _, notes_of_chord in groupby(notes, lambda note: note // 10):
+        chord = 0
+        for note in notes_of_chord:
+            chord |= (1 << (note % 10))
+        chords.append(chord)
+    return chords
+
+def chord_to_str(chord):
+    scratch_str = 'S' if chord & 1 == 1 else ' '
+    keys = chord >> 1
+    keys_str = reversed_str(f'{keys:07b}')\
+        .replace('1', '|')\
+        .replace('0', '_')
+    return scratch_str + keys_str
+
+def reversed_str(s):
+    return s[::-1]
