ccd37c0610bd0be2679f5d493c0219281827eac0
ikngtty <ikngtty@gmail.com>
2024-09-16 03:38:52 +0900

    内部向けの関数や定数を気持ち非公開に

--- iidx_notes_analyzer/persistence.py
+++ iidx_notes_analyzer/persistence.py
@@ -5,33 +5,33 @@ from typing import List
 from .textage_scraper import iidx
 from .textage_scraper.url import ScorePageParams
 
-DATA_DIR_PATH = 'data'
-SCORE_PAGES_FILE_PATH = os.path.join(DATA_DIR_PATH, 'score_pages.json')
+_DATA_DIR_PATH = 'data'
+_SCORE_PAGES_FILE_PATH = os.path.join(_DATA_DIR_PATH, 'score_pages.json')
 
 # TODO: 保存先ファイルが重複した時にどうするか
 
 def save_score_pages(scores: List[ScorePageParams]):
-    os.makedirs(DATA_DIR_PATH, exist_ok=True)
+    os.makedirs(_DATA_DIR_PATH, exist_ok=True)
 
-    with open(SCORE_PAGES_FILE_PATH, 'w') as f:
+    with open(_SCORE_PAGES_FILE_PATH, 'w') as f:
         json.dump(scores, f)
 
 def load_score_pages() -> List[ScorePageParams]:
-    with open(SCORE_PAGES_FILE_PATH) as f:
+    with open(_SCORE_PAGES_FILE_PATH) as f:
         score_page_param_lists = json.load(f)
 
     return [
         ScorePageParams(*param_list) for param_list in score_page_param_lists
     ]
 
-def get_notes_dir_path(play_side: iidx.PlaySide, version: str) -> str:
-    return os.path.join(DATA_DIR_PATH, 'notes', play_side, version)
+def _get_notes_dir_path(play_side: iidx.PlaySide, version: str) -> str:
+    return os.path.join(_DATA_DIR_PATH, 'notes', play_side, version)
 
-def get_notes_file_path(
+def _get_notes_file_path(
     play_side: iidx.PlaySide, version: str,
     song_id: str, score_kind: iidx.ScoreKind,
 ) -> str:
-    dir = get_notes_dir_path(play_side, version)
+    dir = _get_notes_dir_path(play_side, version)
     filename = f'{song_id}({score_kind}).json'
     return os.path.join(dir, filename)
 
@@ -40,9 +40,9 @@ def save_notes(
     song_id: str, score_kind: iidx.ScoreKind,
     notes: List[int],
 ) -> None:
-    os.makedirs(get_notes_dir_path(play_side, version), exist_ok=True)
+    os.makedirs(_get_notes_dir_path(play_side, version), exist_ok=True)
 
-    save_file_path = get_notes_file_path(
+    save_file_path = _get_notes_file_path(
         play_side, version, song_id, score_kind
     )
     if os.path.exists(save_file_path):
@@ -55,7 +55,7 @@ def load_notes(
     play_side: iidx.PlaySide, version: str,
     song_id: str, score_kind: iidx.ScoreKind,
 ) -> List[int]:
-    load_file_path = get_notes_file_path(
+    load_file_path = _get_notes_file_path(
         play_side, version, song_id, score_kind
     )
     with open(load_file_path) as f:
